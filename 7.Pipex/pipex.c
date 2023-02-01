39ec867da2051ed73f35d1128f7c32236c2474aa
Nicolas GRIVEAU <nicolasgri45@icloud.com>
2023-02-01 10:57:51 +0100

    01/02.10:57

--- 7.Pipex/pipex.c
+++ 7.Pipex/pipex.c
@@ -6,7 +6,7 @@
 /*   By: ngriveau <ngriveau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/31 20:20:06 by ngriveau          #+#    #+#             */
-/*   Updated: 2023/01/31 21:43:49 by ngriveau         ###   ########.fr       */
+/*   Updated: 2023/02/01 10:56:14 by ngriveau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int ft_exe_cmd(t_pip *s, int nbcmd)
 	char **cmd;
 
 	// fprintf(stderr, "s->av[%d] = |%s|\n", nbcmd, s->av[nbcmd]);
-	cmd = ft_split(s->av[nbcmd], ' ');	
+	cmd = ft_split(s->av[nbcmd], ' ');
 	i = -1;
 	// while (cmd[++i])
 	// 	fprintf(stderr, "cmd[%d] = |%s|\n", i, cmd[i]);
@@ -30,7 +30,7 @@ int ft_exe_cmd(t_pip *s, int nbcmd)
 	// fprintf(stderr, "marche pas");
 }
 
-int main(int ac, char **av, char *envp[])
+int main(int ac, char **av, char **envp)
 {
 	t_pip s;
 	int i;
@@ -49,10 +49,13 @@ int main(int ac, char **av, char *envp[])
 	// i = -1;
 	// while (path[++i])
 	// 	printf("path = %s\n", path[i]);
-	id = fork();
 	printf("id = %d\n\n\n", id);
+	id = fork();
 	if (id == 0)
+	{
+		wait(2);
 		ft_exe_cmd(&s, 1);
-	else
-		ft_exe_cmd(&s, 2);
+	}
+	ft_exe_cmd(&s, 2);
+	fprintf(stderr, "salut les amis");
 }
